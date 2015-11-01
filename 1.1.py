3fa7a532018607e213fc37b12bc1d542ed9d2fa3
fede2001 <fede2001@hotmail.com>
2015-11-01 11:35:41 -0500

    Reestructuro contenidos

---
+++ 1.1.py
@@ -0,0 +1,3 @@
+Recursos=
+        "Algo que se tiene y se va a trabajar con ello, algo cosa que pueda ser usada en el proceso, inventarios, incluyendo tiempo, materiales físicos, otras personas, ideas, limitaciones, motivaciones, metas etc."
+}
