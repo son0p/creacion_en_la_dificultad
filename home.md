f8498aab487f09feb4d6bf9c949ac02fa2c9c219
fede2001 <fede2001@hotmail.com>
2015-11-03 11:39:09 -0500

    actualizo contenido

---
+++ home.md
@@ -0,0 +1,16 @@
+
+
+
+[0.0 Contexto](0.0.md)
+
+[1.0 Resources](1.0.md)
+
+[2.0 Score](2.0.md)
+
+[3.0 Valuaction](3.0.md)
+
+[4.0 Performance](4.0.md)
+
+[1.0 Recursos](1.0.md)
+
+<img src="images/home_adapting_to_the_outside.jpg" height="250" width="400">
---
+++ ruta.py
@@ -0,0 +1,82 @@
+def Ruta = {
+    a = Cuestionar todo para construir sus valores, cuestionables;
+    b = Inteligencia creciente;
+    c = Misterio, complejidad, método;
+}
+
+"No importa como lo inflen los medios o los seguidores ... su unidad de trabajo es..._?"
+
+
+
+};
+# ======= CONTEXTO ===========
+
+def Momentum():
+    teoria="Período de tiempo donde se mantienen unas condiciones favorables para las acciones determinadas"
+
+
+
+def QuienSoy;
+# ACCION: ¿Quien soy => ¿en qué quisiera usar tiempo libre?, ¿A qué le temo?
+
+
+def Escala {
+  Localism
+  # A=Redonditos de Ricota
+}
+
+def ObjetoA ={"Mi producto te facilita..."};
+
+
+
+
+
+
+# ======= 1 ===============
+def Contrincante
+# AN= Haikido
+
+def Roles():
+  "La sociedad del Espectáculo"
+
+
+def Grupo_de_trabajo
+1. Forming: The group comes together and gets to initially know one other and form as a group.
+2. Storming: A chaotic vying for leadership and trialling of group processes
+3. Norming: Eventually agreement is reached on how the group operates (norming)
+4. Performing: El grupo practica su oficio, se vuelve efectivo en reuniones y objetivos.
+5. Adjourning: The process of "unforming" the group, that is, letting go of the group structure and moving on.
+
+def tiempo():
+  A="Cultura de las estaciones, ciclos en la naturaleza"
+  H=""
+
+
+"contenidos":{
+    "restricciones":{
+        "definicón":"externas",
+        "herramientas":{
+            "evasión":{
+                "rodear":"",
+                "superar":"",
+                "evitar":"",
+            }
+}
+    }
+    "distracciones":{
+        "definición":"internas",
+"casos":{
+    "ideales":"".
+
+}
+        "herramientas":{
+            "metodo":{
+                "estructurar":"partes, mapas mentales, tareas",
+                "tiempos":"10mins facebook"
+            }
+        }
+    },
+    "fracaso":"",
+    "complejidad":"",
+"ideales"
+}
